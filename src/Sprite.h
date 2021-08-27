2501b1894bd99c98f5407a726de2f47348e854e7
Gustavo Barros <gustavoasb@gmail.com>
2021-08-27 09:18:54 -0300

    ✨ Create sprite class

---
+++ src/Sprite.h
@@ -0,0 +1,21 @@
+#include <string>
+#include <SDL2/SDL.h>
+using namespace std;
+
+class Sprite{
+  public:
+    Sprite();
+    Sprite(string file);
+    ~Sprite();
+    void Open(string file);
+    void SetClip(int x, int y, int w, int h);
+    void Render(int x, int y);
+    int GetWidth();
+    int GetHeight();
+    bool IsOpen();
+  private:
+    SDL_Texture* texture;
+    int width;
+    int height;
+    SDL_Rect clipRect;
+};
