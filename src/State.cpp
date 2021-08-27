1f024220e6321ebcad4006d79e3d805078869eed
Gustavo Barros <gustavoasb@gmail.com>
2021-08-27 09:14:47 -0300

    ✨Implement State functions

---
+++ src/State.cpp
@@ -0,0 +1,28 @@
+#define INCLUDE_SDL_IMAGE
+#define INCLUDE_SDL_MIXER
+#include "State.h"
+#include "SDL_include.h"
+
+State::State(){
+  this->QuitRequested();
+  
+  // Instancia Sprite 
+}
+
+void State::LoadAssets(){
+  // inicializar assets aqui sempre q possivel
+}
+
+void State::Update(float dt){
+  if(SDL_QuitRequested()){
+    this->quitRequested = true;
+  }
+}
+
+void State::Render(){
+  //chamar render do fundo
+}
+
+bool State::QuitRequested(){
+  return this->quitRequested;
+}
