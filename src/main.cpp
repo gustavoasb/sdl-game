5e10fd6ca4fc76748cabe1d1b656407861de1d5b
Gustavo Barros <gustavoasb@gmail.com>
2021-08-27 22:09:04 -0300

    ✨ Run game

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include "Game.h"
 
-int main(int argc, char *argv[]) {
-
-
+int main(int argc, char** argv) {
+  Game game = Game::GetInstance();
+  game.Run();
   return 0;
-
 }
