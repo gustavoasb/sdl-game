009e2d05cd8d24a07abf9fed3eba3bb6f5fff781
Gustavo Barros <gustavoasb@gmail.com>
2021-09-13 20:22:22 -0300

    ♻️ Update sprite to use component

--- src/Sprite.h
+++ src/Sprite.h
@@ -5,19 +5,22 @@
 #include <SDL2/SDL.h>
 #define INCLUDE_SDL_IMAGE
 #include "SDL_include.h"
+#include "Component.h"
 using namespace std;
 
-class Sprite{
+class Sprite : public Component{
   public:
-    Sprite();
-    Sprite(string file);
+    Sprite(GameObject& associated);
+    Sprite(GameObject& associated, string file);
     ~Sprite();
     void Open(string file);
     void SetClip(int x, int y, int w, int h);
-    void Render(int x, int y);
     int GetWidth();
     int GetHeight();
     bool IsOpen();
+    void Update(float dt);
+    void Render();
+    bool Is(string type);
   private:
     SDL_Texture* texture;
     int width;
