16927a8edd10ec60e95c38091bd76b7cbba5f942
Gustavo Barros <gustavoasb@gmail.com>
2021-09-13 23:09:32 -0300

    🐛 Change times that sound plays

--- src/Sound.h
+++ src/Sound.h
@@ -13,7 +13,7 @@ class Sound : public Component{
     Sound(GameObject& associated);
     Sound(GameObject& associated, string file);
     ~Sound();
-    void Play(int times = -1);
+    void Play(int times = 1);
     void Stop();
     void Open(string file);
     bool IsOpen();
