f1e56222788b71537f17ef859b9fb0a63d1b4c58
Gustavo Barros <gustavoasb@gmail.com>
2021-11-05 10:50:08 -0300

    ✨ Implement TileMap

---
+++ src/TileMap.cpp
@@ -0,0 +1,80 @@
+#include "TileMap.h"
+#include <fstream>
+#include <iostream>
+
+TileMap::TileMap(GameObject& associated, std::string file, TileSet* tileSet)  : Component(associated){
+  this->Load(file);
+  this->SetTileSet(tileSet);
+}
+
+void TileMap::Load(std::string file){
+  ifstream stream(file, ios::in);
+
+  if (!stream.is_open())
+  {
+    std::cout << "Error loading file " << file << std::endl;
+    exit(1);
+  }
+
+  char c;
+
+  stream >> mapWidth >> c >> mapHeight >> c >> mapDepth >> c;
+
+  int in;
+  while (stream >> in)
+  {
+    tileMatrix.push_back(in - 1);
+    stream >> c;
+  }
+}
+
+void TileMap::SetTileSet(TileSet* tileSet){
+  this->tileSet = tileSet;
+}
+
+int& TileMap::At(int x, int y, int z){
+  int idx = z * mapWidth * mapHeight + y * mapWidth + x;
+  return tileMatrix[idx];
+}
+
+void TileMap::Render(){
+  // i = layer
+  for (int i = 0; i < mapDepth; i++){
+    RenderLayer(i, 0, 0);
+  }
+}
+
+void TileMap::RenderLayer(int layer, int cameraX, int cameraY){
+  for (int y = 0; y < mapHeight; y++){
+    for (int x = 0; x < mapWidth; x++){
+      int xPos = x * tileSet->GetTileWidth() - cameraX;
+      int yPos = y * tileSet->GetTileHeight() - cameraY;
+      tileSet->RenderTile(At(x, y, layer), xPos, yPos);
+    }
+  }
+}
+
+int TileMap::GetWidth(){
+  return this->mapWidth;
+}
+
+int TileMap::GetHeight(){
+  return this->mapHeight;
+}
+
+int TileMap::GetDepth(){
+  return this->mapDepth;
+}
+
+bool TileMap::Is(string type)
+{
+  return "TileMap" == type;
+}
+
+TileMap::~TileMap(){
+  
+}
+
+void TileMap::Update(float dt){
+
+}
