2127581ce23a244e14cb13666de1a7da10c6c8c2
Gustavo Barros <gustavoasb@gmail.com>
2022-09-23 08:28:03 -0300

    Camera

---
+++ src/Camera.cpp
@@ -0,0 +1,33 @@
+#include "Camera.h"
+#include "InputManager.h"
+
+Vec2 Camera::pos = Vec2(0, 0);
+Vec2 Camera::speed = Vec2(250, 250);
+GameObject *Camera::focus = nullptr;
+
+void Camera::Follow(GameObject* newFocus){
+  Camera::focus = newFocus;
+}
+
+void Camera::Unfollow(){
+  Camera::focus = nullptr;
+}
+
+void Camera::Update(float dt){
+  if(Camera::focus != nullptr){
+    InputManager &input = InputManager::GetInstance();
+
+    if(input.IsKeyDown(UP_ARROW_KEY)) {
+      Camera::pos.y -= speed.y * dt;
+    }
+    if(input.IsKeyDown(DOWN_ARROW_KEY)) {
+      Camera::pos.y += speed.y * dt;
+    }
+    if(input.IsKeyDown(LEFT_ARROW_KEY)) {
+      Camera::pos.x -= speed.x * dt;
+    }
+    if(input.IsKeyDown(RIGHT_ARROW_KEY)) {
+      Camera::pos.x += speed.x * dt;
+    }
+  }
+}
